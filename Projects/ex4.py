554221301d99f242a20e381bc0161ed4b992ea22
Charlie Crewe <s171298@ucs.ac.uk>
2017-09-28 12:15:44 +0100

    Completed the four introductory exercises.

---
+++ Projects/ex4.py
@@ -0,0 +1,16 @@
+my_name = 'Jordan Carman'
+my_age = 19
+my_height = 72 # I think
+my_weight = 210
+my_eyes = 'Blue'
+my_hair = 'Brown'
+is_heavy = my_weight > 3000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} inches tall.")
+print(f"He is {my_weight} pounds heavy.")
+print(f"Is is {is_heavy} that he is overweight.")
+print(f"He has {my_eyes} eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"Is I add {my_age}, {my_height} and {my_weight} I get {total}")
