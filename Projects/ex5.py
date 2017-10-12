36e499b715dc4718013f5b10e435f9596c5be4c4
Tom Gibbs <S184605@ucs.ac.uk>
2017-10-12 10:21:34 +0100

    Week 2 Task 1

---
+++ Projects/ex5.py
@@ -0,0 +1,7 @@
+Bottles = 99
+while Bottles > 1:
+    print(Bottles, "bottles of beer on the wall, ", Bottles, "bottles of beer.")
+    Bottles = Bottles - 1
+    print("Take one down, pass it around, ", Bottles, "bottles of beer on the wall.")
+print("1 bottle of beer on the wall, 1 bottle of beer.")
+print("So take it down, pass it around, no more bottles of beer on the wall!")    
